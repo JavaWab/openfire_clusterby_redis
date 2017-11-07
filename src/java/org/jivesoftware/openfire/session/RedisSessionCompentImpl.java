80c743ed57e43a6b14e017903e6a1d3c4212743c
wanganbang <wanganbang@dyrs.com.cn>
2017-11-08 00:21:59 +0800

    test

--- src/java/org/jivesoftware/openfire/session/RedisSessionCompentImpl.java
+++ src/java/org/jivesoftware/openfire/session/RedisSessionCompentImpl.java
@@ -43,6 +43,7 @@ public class RedisSessionCompentImpl implements RedisSessionCompent {
             jedis.hset("UserSession:" + route.toBareJID(), route.getResource(), jid);
         }
         jedis.close();
+
     }
 
     @Override
